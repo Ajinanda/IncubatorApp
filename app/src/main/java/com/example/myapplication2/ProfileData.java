8350d127ba7ed0d356eff2c504e2f0d99ebf8203
Ajinanda <42225170+Ajinanda@users.noreply.github.com>
2019-06-22 00:31:25 +0800

    update APP

---
+++ app/src/main/java/com/example/myapplication2/ProfileData.java
@@ -0,0 +1,41 @@
+package com.example.myapplication2;
+
+/**
+ *<h1>Data Profile</h1>
+ * class ini dibuat untuk mengirim data ke database firebase
+ *
+ * @author Ajinanda Nugraha Putra
+ * @version 0.1 (alpha)
+ * @since 2019-6-21
+ */
+
+public class ProfileData {
+    public String name;
+    public int minTemp;
+    public int maxTemp;
+    public int minMoist;
+    public int timeIncubation;
+    public int timeRotation;
+    public int rotationCycle;
+
+    /**
+     *
+     * @param name - nama unggas yang profilnya akan dimasukkan ke database.
+     * @param minTemp - minimum temperatur yang dibutuhkan untuk menginkubasi telur.
+     * @param maxTemp - maximum temperatur yang dibutuhkan untuk menginkubasi telur.
+     * @param minMoist - minimum kelembapan yang dibutuhkan untuk menginkubasi telur.
+     * @param timeIncubation - waktu yang diperlukan untuk menginkubasi telur
+     * @param timeRotation - menentukan jumlah hari dimana telur akan dibolak balik
+     * @param rotationCycle - menentukan setiap berapa jam telur akan dibolak balik
+     */
+    public ProfileData(String name, int minTemp, int maxTemp, int minMoist, int timeIncubation, int timeRotation, int rotationCycle){
+        this.name = name;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+        this.minMoist = minMoist;
+        this.timeIncubation = timeIncubation;
+        this.timeRotation = timeRotation;
+        this.rotationCycle = rotationCycle;
+    }
+
+}
--- app/src/main/java/com/example/myapplication2/ProfileForm.java
+++ app/src/main/java/com/example/myapplication2/ProfileForm.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 /**
@@ -16,37 +20,56 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 
 public class ProfileForm extends AppCompatActivity {
-
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_form);
         setTitle("Form Profile Baru");
         Button buttonSubmitProfile = (Button) findViewById(R.id.buttonSubmitProfile);
-        /*
-        EditText formName = (EditText) findViewById(R.id.formName);
-        EditText formMinTemp = (EditText) findViewById(R.id.formMinTemp);
-        EditText formMaxTemp = (EditText) findViewById(R.id.formMaxTemp);
-        EditText formMinMoist = (EditText) findViewById(R.id.formMaxMoist);
-        EditText formTimeIncubation = (EditText) findViewById(R.id.formTimeIncubation);
-        EditText formTimeRotation = (EditText) findViewById(R.id.formTimeRotation);
-        EditText formRotationCycle = (EditText) findViewById(R.id.formRotationCycle);
+
+        final EditText formName = (EditText) findViewById(R.id.formName);
+        final EditText formMinTemp = (EditText) findViewById(R.id.formMinTemp);
+        final EditText formMaxTemp = (EditText) findViewById(R.id.formMaxTemp);
+        final EditText formMinMoist = (EditText) findViewById(R.id.formMinMoist);
+        final EditText formTimeIncubation = (EditText) findViewById(R.id.formTimeIncubation);
+        final EditText formTimeRotation = (EditText) findViewById(R.id.formTimeRotation);
+        final EditText formRotationCycle = (EditText) findViewById(R.id.formRotationCycle);
 
 
-        String name = formName.getText().toString();
-        String minTemp = formMinTemp.getText().toString();
-        String maxTemp = formMaxTemp.getText().toString();
-        String minMoist = formMinMoist.getText().toString();
-        String timeIncubation = formTimeIncubation.getText().toString();
-        String timeRotation = formTimeRotation.getText().toString();
-        String rotationCycle = formRotationCycle.getText().toString();
-        */
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         buttonSubmitProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(startIntent);
+                try{
+                    final String name = formName.getText().toString();
+                    String minTemp = formMinTemp.getText().toString();
+                    String maxTemp = formMaxTemp.getText().toString();
+                    String minMoist = formMinMoist.getText().toString();
+                    String timeIncubation = formTimeIncubation.getText().toString();
+                    String timeRotation = formTimeRotation.getText().toString();
+                    String rotationCycle = formRotationCycle.getText().toString();
+                    final int minTempInt = Integer.valueOf(minTemp);
+                    final int maxTempInt = Integer.valueOf(maxTemp);
+                    final int minMoistInt = Integer.valueOf(minMoist);
+                    final int timeIncubationInt = Integer.valueOf(timeIncubation);
+                    final int timeRotationInt = Integer.valueOf(timeRotation);
+                    final int rotationCycleInt = Integer.valueOf(rotationCycle);
+
+                    ProfileData newProfile = new ProfileData(name, minTempInt, maxTempInt, minMoistInt, timeIncubationInt, timeRotationInt, rotationCycleInt);
+                    databaseReference.child("Profile").child(newProfile.name).setValue(newProfile);
+                    Intent startIntent = new Intent(getApplicationContext(), ProfileActivity.class);
+                    startActivity(startIntent);
+                } catch (Exception e){
+
+                }
+
             }
         });
     }
+    /*public void addProfile(View view){
+        ProfileData newProfile = new ProfileData(name, minTempInt, maxTempInt, minMoistInt, timeIncubationInt, timeRotationInt, rotationCycleInt);
+        databaseReference.child(newProfile.name).setValue(newProfile);
+    }*/
 }
